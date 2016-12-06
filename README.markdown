961a5f2e271d4d9505cb10511f3c483e905a026e
hveldstra <h@veldstra.org>
2016-12-06 17:05:23 +0000

    Update test scripts, add README and LICENSE

---
+++ README.markdown
@@ -0,0 +1,16 @@
+# Socket.io Chatroom Example Load Tests
+
+This repo contains [Artillery](https://artillery.io) scripts for load testing the [chatroom example](https://github.com/socketio/socket.io/tree/master/examples/chat) bundled with Socket.io.
+
+See the blog post: [Load testing Socket.io applications with Artillery](https://artillery.io/blog/load-testing-socketio-with-artillery)
+
+# Usage
+
+1. Clone this repo & `npm install`
+2. Clone and run https://github.com/socketio/socket.io/tree/master/examples/chat
+3. Point `config.target` to a running instance of the chatroom
+4. Run `npm run load`
+
+# License
+
+MPL 2.0
--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 'use strict';
 
 module.exports = {
@@ -10,12 +14,16 @@ const MESSAGES = [
   'how\'s it going?',
   'what a lovely socket.io chatroom',
   'to be or not to be, that is the question',
-  'thanks for all the fish',
-  'hack the plane'
+  'Romeo, Romeo! wherefore art thou Romeo?',
+  'now is the winter of our discontent.',
+  'get thee to a nunnery',
+  'a horse! a horse! my kingdom for a horse!'
 ];
 
 function setMessage(context, events, done) {
+  // pick a message randomly
   const index = Math.floor(Math.random() * MESSAGES.length);
+  // make it available to templates as "message"
   context.vars.message = MESSAGES[index];
   return done();
 }
